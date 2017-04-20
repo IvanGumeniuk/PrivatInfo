f51797c9862711e343995cbd34722609aa94604e
LittleBitUser <van_gum@i.ua>
2017-04-20 21:58:39 +0300

    can find city in Russian

---
+++ app/src/main/java/gumeniuk/privatinfo/GPSTracker.java
@@ -0,0 +1,234 @@
+package gumeniuk.privatinfo;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+
+/**
+ * Created by Ваня on 11.03.2017.
+ */
+
+public class GPSTracker extends Service implements LocationListener {
+    private final Context mContext;
+    private final Activity mActivity;
+
+    // flag for GPS status
+    boolean isGPSEnabled = false;
+
+    // flag for network status
+    boolean isNetworkEnabled = false;
+
+    // flag for GPS status
+    boolean canGetLocation = false;
+
+    Location location; // location
+    double latitude; // latitude
+    double longitude; // longitude
+
+    // The minimum distance to change Updates in meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+
+    // The minimum time between updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
+
+    // Declaring a Location Manager
+    protected LocationManager locationManager;
+
+    public GPSTracker(Context context, Activity activity) {
+        this.mContext = context;
+        this.mActivity = activity;
+        getLocation();
+    }
+
+    public Location getLocation() {
+        try {
+            locationManager = (LocationManager) mContext
+                    .getSystemService(LOCATION_SERVICE);
+
+//            // getting GPS status
+            isGPSEnabled = locationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+//
+//            // getting network status
+            isNetworkEnabled = locationManager
+                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            Log.d("lonlat", "GPS "+String.valueOf(isGPSEnabled));
+            Log.d("lonlat", "Net "+String.valueOf(isNetworkEnabled));
+
+            this.canGetLocation = true;
+
+
+
+            locationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER,
+                    MIN_TIME_BW_UPDATES,
+                    MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+            Log.d("lonlat", "Network");
+            if (locationManager != null) {
+                location = locationManager
+                        .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                if (location != null) {
+                    latitude = location.getLatitude();
+                    longitude = location.getLongitude();
+                    Log.d("lonlat", String.valueOf(latitude) + " NET " + String.valueOf(longitude));
+                }
+                else Log.d("lonlat", String.valueOf(latitude) + " NET == null" + String.valueOf(longitude));
+            }
+
+            // if GPS Enabled get lat/long using GPS Services
+
+            if(!isNetworkEnabled) {
+                locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        MIN_TIME_BW_UPDATES,
+                        MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                Log.d("lonlat", "GPS Enabled");
+                if (locationManager != null) {
+                    location = locationManager
+                            .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    if (location != null) {
+                        latitude = location.getLatitude();
+                        longitude = location.getLongitude();
+                        Log.d("lonlat", String.valueOf(latitude) + " GPS " + String.valueOf(longitude));
+                    } else
+                        Log.d("lonlat", String.valueOf(latitude) + " GPS == null" + String.valueOf(longitude));
+                }
+            }
+
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(mActivity,new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return location;
+    }
+
+
+
+    /**
+     * Stop using GPS listener
+     * Calling this function will stop using GPS in your app
+     * */
+    public void stopUsingGPS() {
+        if (locationManager != null) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+
+                return;
+            }
+            locationManager.removeUpdates(GPSTracker.this);
+        }
+    }
+
+    /**
+     * Function to get latitude
+     * */
+    public double getLatitude(){
+        if(location != null){
+            latitude = location.getLatitude();
+        }
+
+        // return latitude
+        return latitude;
+    }
+
+    /**
+     * Function to get longitude
+     * */
+    public double getLongitude(){
+        if(location != null){
+            longitude = location.getLongitude();
+        }
+
+        // return longitude
+        return longitude;
+    }
+
+    /**
+     * Function to check GPS/wifi enabled
+     * @return boolean
+     * */
+    public boolean canGetLocation() {
+        return this.canGetLocation;
+    }
+
+    /**
+     * Function to show settings alert dialog
+     * On pressing Settings button will lauch Settings Options
+     * */
+    public void showSettingsAlert(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+
+        // Setting Dialog Title
+        alertDialog.setTitle("GPS is settings");
+
+        // Setting Dialog Message
+        alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
+
+        // On pressing Settings button
+        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog,int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                mContext.startActivity(intent);
+            }
+        });
+
+        // on pressing cancel button
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        // Showing Alert Message
+        alertDialog.show();
+    }
+
+    public void isGpsEnabled(){
+        getLocation();
+        if(!isGPSEnabled && !isNetworkEnabled) showSettingsAlert();
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        Log.d("lonlat", "location changet "+ String.valueOf(latitude)+" "+String.valueOf(longitude));
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+}
--- app/src/main/java/gumeniuk/privatinfo/MainActivity.java
+++ app/src/main/java/gumeniuk/privatinfo/MainActivity.java
@@ -1,70 +1,240 @@
 package gumeniuk.privatinfo;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 import java.util.ArrayList;
 
+import gumeniuk.privatinfo.GoogleMapApi.TownRequest;
+import gumeniuk.privatinfo.PrivatApi.CashMashines;
 import gumeniuk.privatinfo.PrivatApi.Device;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    double latitude;
+    double longitude;
+    GoogleMap mMap;
+    private ArrayList<Device> atms;
+    private ArrayList<Device> tsos;
+    private ProgressDialog mProgressDialog;
+
+    public ArrayList<Device> getAtms() {
+        return atms;
+    }
+
+    public void setAtms(ArrayList<Device> atms) {
+        this.atms = atms;
+    }
+
+    public ArrayList<Device> getTsos() {
+        return tsos;
+    }
+
+    public void setTsos(ArrayList<Device> tsos) {
+        this.tsos = tsos;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         final MyApp app = (MyApp) this.getApplicationContext();
 
-        final ArrayList<Device> atms = new ArrayList<>();
-        final ArrayList<Device> tsos = new ArrayList<>();
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
 
         Button button = (Button) findViewById(R.id.button);
         Button button2 = (Button) findViewById(R.id.button2);
 
-/*
-        MyApp.getAtmService().getData("", "", "Львов").enqueue(new Callback<CashMashines>() {
+        atms = new ArrayList<>();
+        tsos = new ArrayList<>();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getUserLocation();
+            }
+        });
+
+        getUserLocation();
+    }
+
+
+    private void privatRequest(String city) {
+        MyApp.getAtmService().getData("", "", city).enqueue(new Callback<CashMashines>() {
             @Override
             public void onResponse(Call<CashMashines> call, Response<CashMashines> response) {
-                if (response.body().getDevices() != null)
-                    atms.addAll(response.body().getDevices());
+                hideProgressDialog();
+                Toast.makeText(MainActivity.this, "atms size in resp " + response.body().getDevices().size(), Toast.LENGTH_LONG).show();
+                atms.addAll(response.body().getDevices());
             }
 
             @Override
             public void onFailure(Call<CashMashines> call, Throwable t) {
-                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+                hideProgressDialog();
+                Toast.makeText(MainActivity.this, "ATM finding error. Check internet connection", Toast.LENGTH_LONG).show();
             }
 
         });
-        MyApp.getTsoService().getData("", "", "Львов").enqueue(new Callback<CashMashines>() {
+
+        MyApp.getTsoService().getData("", "", city).enqueue(new Callback<CashMashines>() {
             @Override
             public void onResponse(Call<CashMashines> call, Response<CashMashines> response) {
-                if (response.body().getDevices() != null)
-                    tsos.addAll(response.body().getDevices());
+                Toast.makeText(MainActivity.this, "tsos size in resp " + response.body().getDevices().size(), Toast.LENGTH_LONG).show();
+                tsos.addAll(response.body().getDevices());
+                createMarkers();
             }
 
             @Override
             public void onFailure(Call<CashMashines> call, Throwable t) {
-                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+                hideProgressDialog();
+                Toast.makeText(MainActivity.this, "TSO finding error. Check internet connection", Toast.LENGTH_LONG).show();
             }
         });
-        MyApp.getLocationWorking().getData("49.53245628,23.97216106", "ru").enqueue(new Callback<TownRequest>() {
-            @Override
-            public void onResponse(Call<TownRequest> call, Response<TownRequest> response) {
-                Toast.makeText(MainActivity.this, String.valueOf(response.body().getResults().get(1).getAddressComponents().get(0).getShortName()), Toast.LENGTH_SHORT).show();
-            }
 
-            @Override
-            public void onFailure(Call<TownRequest> call, Throwable t) {
-                Toast.makeText(MainActivity.this, "wtf", Toast.LENGTH_SHORT).show();
-            }
-        });
-*/
+    }
+
+    private void createMarkers() {
+
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        if (ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+
+        } else {
+            Toast.makeText(this, "You have to accept to enjoy all app's services!", Toast.LENGTH_LONG).show();
+        }
 
 
     }
 
+    private void showLatLng() {
+        Toast.makeText(this, latitude + " " + longitude, Toast.LENGTH_LONG).show();
+    }
+
+    private void getUserLocation() {
+        GPSTracker gps = new GPSTracker(this, this);
+
+        if (gps.canGetLocation()) {
+            latitude = gps.getLatitude();
+            longitude = gps.getLongitude();
+            Toast.makeText(this, latitude + " " + longitude, Toast.LENGTH_LONG).show();
+
+            loadData();
+        } else {
+            gps.showSettingsAlert();
+        }
+    }
+
+    private void loadData() {
+
+        MyApp.getLocationWorking().getData(latitude + "," + longitude, "ru").enqueue(new Callback<TownRequest>() {
+            @Override
+            public void onResponse(Call<TownRequest> call, Response<TownRequest> response) {
+                for (int i = 1; i < response.body().getResults().size(); i++)
+                    if (response.body().getResults().get(i).getTypes().get(0).equals("postal_code")) {
+                        Toast.makeText(MainActivity.this, String.valueOf(response.body().getResults().get(i).getAddressComponents().get(1).getShortName().trim()), Toast.LENGTH_SHORT).show();
+                        privatRequest(response.body().getResults().get(i).getAddressComponents().get(1).getShortName().trim());
+                        break;
+                    } else if (i == response.body().getResults().size() - 1) {
+                        hideProgressDialog();
+                        Toast.makeText(MainActivity.this, "Не вдалось знайти місто", Toast.LENGTH_SHORT).show();
+                    }
+            }
+
+            @Override
+            public void onFailure(Call<TownRequest> call, Throwable t) {
+                hideProgressDialog();
+                Toast.makeText(MainActivity.this, "Something is wrong. Check internet connection", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void showProgressDialog(Context context) {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(context);
+            mProgressDialog.setMessage("Loading...");
+            mProgressDialog.setIndeterminate(true);
+        }
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    /*  private void showStartDialog() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("City name");
+
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        final EditText inputTitle = new EditText(this);
+
+        inputTitle.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PERSON_NAME);
+        inputTitle.setHint("City name in russian");
+        inputTitle.setHintTextColor(ContextCompat.getColor(this, R.color.grey));
+
+        layout.addView(inputTitle);
+        builder.setView(layout);
+
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (!inputTitle.getText().toString().trim().isEmpty() && validCity(inputTitle.getText().toString().trim())) {
+                    privatRequest(inputTitle.getText().toString().trim());
+                    showProgressDialog(MainActivity.this);
+                } else {
+                    Toast.makeText(MainActivity.this, "Please, enter sity name", Toast.LENGTH_SHORT).show();
+                    showStartDialog();
+                }
+            }
+        });
+
+        builder.setNeutralButton("Use current location", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                getUserLocation();
+                dialog.dismiss();
+                showProgressDialog(MainActivity.this);
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+
+        final AlertDialog dialog = builder.create();
+        dialog.show();
+    }*/
 
 }
